cc9556753c98a474c284559f2e1c0b330fddbc30
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2025-04-09 22:44:23 +0530

    [Naman Malhotra] ADD: Merge two JSON objects into one

---
+++ src/main/java/com/bridgelabz/practiceproblems/MergeTwoJson.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.practiceproblems;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+public class MergeTwoJson {
+    public static void main(String[] args) {
+        JsonObject car = new JsonObject();
+        JsonObject mainObject = new JsonObject();
+        JsonArray carArray = new JsonArray();
+
+        car.addProperty("title", "Mazda MX-5" );
+        car.addProperty("start_production", "1989");
+        car.addProperty("class","Sports car Roadster");
+
+        JsonObject car2 = new JsonObject();
+        car2.addProperty("title", "Volkswagen Kübelwagen" );
+        car2.addProperty("start_production", "1995");
+        car2.addProperty("class","Military vehicle");
+
+        carArray.add(car);
+        carArray.add(car2);
+
+        mainObject.add("Cars:", carArray);
+        System.out.println(mainObject.toString());
+    }
+}
